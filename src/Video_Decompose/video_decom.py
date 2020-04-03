e5269841a23c5b015f5487fde29a05d0228297e5
Teo <tp4348@student.uni-lj.si>
2020-04-04 01:19:52 +0200

    added methods to VideoCV class

--- src/Video_Decompose/video_decom.py
+++ src/Video_Decompose/video_decom.py
@@ -1,9 +1,9 @@
+import os
 import numpy as np
 import signal
 import cv2 as cv
 import imutils
 from fps_thread import BoostedFPS
-import os
 
 
 cv.namedWindow('image', cv.WINDOW_NORMAL)
@@ -13,10 +13,6 @@ class VideoCV(object):
 	def __init__(self):
 		self.frame = None
 		self.boostfps = BoostedFPS('test_countryroad.mp4')
-				
-		self.init_video()
-
-	def init_video(self):
 		self.bstream = self.boostfps.startStream()
 
 	def exit_program(self, *args):
@@ -24,29 +20,28 @@ class VideoCV(object):
 		self.bstream.stopStream()
 		os.system('pkill -9 python')	
 
-def extractCorner(img):
-	gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-	extracted = cv.cornerHarris(gray, 2, 11, 0.04)
+	def extractCorner(self, img):
+		gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+		extracted = cv.cornerHarris(gray, 2, 11, 0.04)
 
-	return extracted
+		return extracted
 
-def threshHSV(img):
-	img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
+	def threshHSV(self, img):
+		img_hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 
-	lower_thresh = np.array([0,50,50])
-	upper_thresh = np.array([50,255,255])
-	threshed = cv.inRange(img_hsv, lower_thresh, upper_thresh)
-	konjugated = cv.bitwise_and(img, img, mask=threshed)
-	return konjugated
-	
-def process_frame(img):
-	try:
-		processed_img = extractCorner(img)
+		lower_thresh = np.array([0,50,50])
+		upper_thresh = np.array([50,255,255])
+		threshed = cv.inRange(img_hsv, lower_thresh, upper_thresh)
+		konjugated = cv.bitwise_and(img, img, mask=threshed)
+		
+		return konjugated
+		
+	def process_frame(self, img):
+		processed_img = self.threshHSV(img)
 		cv.imshow('image', img)
 		cv.imshow('filtered', processed_img)
 		cv.waitKey(1)
-	except:
-		pass
+
 
 if __name__ == '__main__':
 	videocv = VideoCV()
@@ -55,7 +50,7 @@ if __name__ == '__main__':
 	running = True
 	while running:
 		videocv.frame = videocv.bstream.getFrame()
-		process_frame(videocv.frame)
+		videocv.process_frame(videocv.frame)
 		running = videocv.bstream.checkBuffer()		
 
 	cv.destroyAllWindows()
