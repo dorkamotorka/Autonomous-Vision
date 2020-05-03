9ebee1f5df90c8e561a38e7b6c6bd2e8b3964c90
Teo <tp4348@student.uni-lj.si>
2020-05-03 20:48:46 +0200

    added param configer

--- src/Extract_Feature/feats_extra.py
+++ src/Extract_Feature/feats_extra.py
@@ -1,7 +1,9 @@
 import os
 import sys
 sys.path.append('..')
+sys.path.append('../GUI')
 from logger import Logger
+from dyconfigure import DynamicConfigure
 import numpy as np
 import signal
 import cv2 as cv
@@ -14,69 +16,70 @@ cv.namedWindow('filtered', cv.WINDOW_NORMAL)
 log = Logger('Feats_Extract')
 
 class FeatureExtract(object):
-	def __init__(self):
-		self.fast = cv.FastFeatureDetector_create(threshold=10, nonmaxSuppression=True, type=cv.FAST_FEATURE_DETECTOR_TYPE_9_16) 	
-		self.orb = cv.ORB_create(nfeatures=700, scaleFactor=1.5, nlevels=3, edgeThreshold=31, firstLevel=0, WTA_K=2, scoreType=cv.ORB_HARRIS_SCORE, patchSize=31, fastThreshold=20)
-		self.bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=False)
-		signal.signal(signal.SIGINT, self.exit_program)
-		self.last = None
-		self.filter_img = None
+    def __init__(self):
+        self.fast = cv.FastFeatureDetector_create(threshold=10, nonmaxSuppression=True, type=cv.FAST_FEATURE_DETECTOR_TYPE_9_16) 	
+        self.orb = cv.ORB_create(nfeatures=700, scaleFactor=1.5, nlevels=3, edgeThreshold=31, firstLevel=0, WTA_K=2, scoreType=cv.ORB_HARRIS_SCORE, patchSize=31, fastThreshold=20)
+        self.bf = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=False)
+        self.cfg = DynamicConfigure()
+        signal.signal(signal.SIGINT, self.exit_program)
+        self.last = None
+        self.filter_img = None
 
-	def exit_program(self, *args): # put in slam
-		log.info("Exiting the program!")
-		os.system('pkill -9 python')	
+    def exit_program(self, *args): # put in slam
+        log.info("Exiting the program!")
+        os.system('pkill -9 python')	
 
-	def detectCombo(self, img):
-		blur = cv.GaussianBlur(img, (5,5), 0)
-		# extract
-		kp = self.fast.detect(blur, mask=None)
-		#kp = [cv.KeyPoint(x=kps.pt[0], y=kps.pt[1], _size=20) for kps in kp] # adjust _size
-		# describe
-		kp, des = self.orb.compute(blur, kp)
-		# match
-		good, matches = [], []
-		if self.last is not None:
-			matches = self.bf.knnMatch(des, self.last['des'], k=2)
-			matches = np.array(matches)
-			print(f"MATCHES: {matches.shape[0]}")
-			# Lowe's ratio
-			for m,n in matches:
-				if m.distance < 0.75*n.distance:
-					kp1 = kp[m.queryIdx].pt
-					kp2 = self.last['kps'][m.trainIdx].pt
-					good.append((kp1, kp2))
+    def detectCombo(self, img):
+        blur = cv.GaussianBlur(img, (5,5), 0)
+        # extract
+        kp = self.fast.detect(blur, mask=None)
+        #kp = [cv.KeyPoint(x=kps.pt[0], y=kps.pt[1], _size=20) for kps in kp] # adjust _size
+        # describe
+        kp, des = self.orb.compute(blur, kp)
+        # match
+        good, matches = [], []
+        if self.last is not None:
+            matches = self.bf.knnMatch(des, self.last['des'], k=2)
+            matches = np.array(matches)
+            print(f"MATCHES: {matches.shape[0]}")
+            # Lowe's ratio
+            for m,n in matches:
+                if m.distance < 0.75*n.distance:
+                    kp1 = kp[m.queryIdx].pt
+                    kp2 = self.last['kps'][m.trainIdx].pt
+                    good.append((kp1, kp2))
 
-		if len(good) > 0:
-			good = np.array(good)
-			self.normalize(good)
-			print(f"GOOD: {good.shape[0]}")	
-			# RANSAC	
-			model, inliers = ransac((good[:, 0], good[:, 1]),
-						FundamentalMatrixTransform,
-						min_samples=8,
-						residual_threshold=1,
-						max_trials=100)
-			good = good[inliers]
-			#print(good)
-			
-		self.last = {'kps': kp, 'des': des}			
-		self.filter_img = cv.drawKeypoints(img, keypoints=kp, outImage=None, color=(255,0,0))
-		kp = np.array([(kps.pt[0], kps.pt[1]) for kps in kp])
+        if len(good) > 0:
+            good = np.array(good)
+            self.normalize(good)
+            print(f"GOOD: {good.shape[0]}")	
+            # RANSAC	
+            model, inliers = ransac((good[:, 0], good[:, 1]),
+                                    FundamentalMatrixTransform,
+                                    min_samples=8,
+                                    residual_threshold=1,
+                                    max_trials=100)
+            good = good[inliers]
+            #print(good)
+            
+        self.last = {'kps': kp, 'des': des}			
+        self.filter_img = cv.drawKeypoints(img, keypoints=kp, outImage=None, color=(255,0,0))
+        kp = np.array([(kps.pt[0], kps.pt[1]) for kps in kp])
 
-		return kp, des, matches
+        return kp, des, matches
 
 
-	def normalize(self, norm_arr): # put in helpers
-		maxpix = np.amax(norm_arr)
-		norm_arr = norm_arr/maxpix # FIX?
-		print(norm_arr)
-		
-	def denormalize(self, denom_arr):
-		
+    def normalize(self, norm_arr): # put in helpers
+        maxpix = np.amax(norm_arr)
+        norm_arr = norm_arr/maxpix # FIX?
+        print(norm_arr)
 
-	def process_frame(self, img):
-		cv.imshow('image', img)
-		if self.filter_img is not None:
-			cv.imshow('filtered', self.filter_img)
-		cv.waitKey(1)
-	
+    def denormalize(self, denom_arr):
+        pass		
+
+    def process_frame(self, img):
+        cv.imshow('image', img)
+        if self.filter_img is not None:
+            cv.imshow('filtered', self.filter_img)
+        cv.waitKey(1)
+
---
+++ src/GUI/dyconfigure.py
@@ -0,0 +1,31 @@
+import numpy as np
+import cv2 as cv
+from threading import Thread
+
+def nothing():
+    pass
+
+class DynamicConfigure(object):
+    def __init__(self, window):
+        self.win = window
+        cv.createTrackbar('NumKeyPoints', self.win, 100, 1000, nothing)
+        #cv.createTrackbar('NumKeyPoints', self.win, 100, 1000, nothing)
+        #cv.createTrackbar('NumKeyPoints', self.win, 100, 1000, nothing)
+        switch = '0 : OFF \n1 : ON'
+        cv.createTrackbar(switch, self.win, 0,1, nothing)
+        Thread(target=self.track, args=(), daemon=True).start() 
+
+
+
+    def track(self):
+        num = cv.getTrackbarPos('NumKeyPoints', self.win)
+        toggle = cv.getTrackbarPos(switch, self.win)
+
+        if toggle == 0:
+            img[:] = 0
+        else:
+            img[:] = [num]
+
+
+
+
