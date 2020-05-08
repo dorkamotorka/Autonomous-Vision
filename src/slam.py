516a4ca1486d59426fb38c71d9e1cdf898d06617
Teo <tp4348@student.uni-lj.si>
2020-05-08 22:35:01 +0200

    Created main function.

--- src/slam.py
+++ src/slam.py
@@ -7,54 +7,19 @@ sys.path.append('Extract_Feature')
 from qbooster import BoostedFPS
 from feats_extra import FeatureExtract
 sys.path.append('GUI')
-#from pangopango import Pango3D
 import cv2 as cv
 import numpy as np
 
 
-
 running = True
-win = 'Processed'
-trackbar = 'Configuration'
-cv.namedWindow('image', cv.WINDOW_NORMAL)
-cv.namedWindow(win, cv.WINDOW_NORMAL)
-cv.namedWindow(trackbar, flags=cv.WINDOW_AUTOSIZE)
-
-def HomogenousCoord(pts):
-    # [x,y] -> [x,y,1]
-    pts = np.append(pts, np.ones([pts.shape[0], 1]), axis=1)# commented numpy array in feats_extract
-    return pts
-
+#trackbar = 'Configuration'
+cv.namedWindow('Original', cv.WINDOW_NORMAL)
+cv.namedWindow('Processed', cv.WINDOW_NORMAL)
+#cv.namedWindow(trackbar, flags=cv.WINDOW_AUTOSIZE)
 
 log = Logger('slam')
 
-class Slam(object):
-    def __init__(self):
-        self.feats = FeatureExtract()
-        #self.pango3d = Pango3D()
-        self.booster = BoostedFPS('test_countryroad.mp4')
-        cv.createTrackbar('NumKeyPoints', trackbar, 100, 1000, lambda nothing : nothing)
-
-    def process_frame(self, img):
-        cv.imshow('image', img) 
-        num = cv.getTrackbarPos('NumKeyPoints', trackbar)
-        self.feats.ParamsCallback(num)
-        _kp, _des, _matches = self.feats.detectCombo(img)
-        cv.drawKeypoints(img, keypoints=_kp, outImage=img, color=(255,0,0))
-        cv.imshow(win, img)
-        cv.waitKey(1)
-
-
-    def exit_program(self, *args):
-        global running
-        print("Exiting the program!")
-        os.system('pkill -9 python')
-        self.booster.stopStream()
-        running = False
-
-
-#put in main
-if __name__ == '__main__':
+def main():
     slam = Slam()
     signal.signal(signal.SIGINT, slam.exit_program)
     start = cv.getTickCount()	
@@ -68,3 +33,34 @@ if __name__ == '__main__':
     exec_time = (end - start)/ cv.getTickFrequency()
     log.info(f"Execution time: {exec_time} seconds")
     cv.destroyAllWindows()
+
+
+
+class Slam(object):
+    def __init__(self):
+        self.feats = FeatureExtract()
+        self.booster = BoostedFPS('test_countryroad.mp4')
+        #cv.createTrackbar('NumKeyPoints', trackbar, 100, 1000, lambda nothing : nothing)
+
+    def process_frame(self, img):
+        cv.imshow('image', img) 
+        #num = cv.getTrackbarPos('value_name', trackbar)
+        #self.feats.ParamsCallback(num)
+        _kp, _des, _matches = self.feats.detectCombo(img)
+        cv.drawKeypoints(img, keypoints=_kp, outImage=img, color=(255,0,0))
+        cv.imshow('Processed', img)
+        cv.waitKey(1)
+
+
+    def exit_program(self, *args):
+        global running
+        print("Exiting the program!")
+        os.system('pkill -9 python')
+        self.booster.stopStream()
+        running = False
+
+
+#put in main
+if __name__ == '__main__':
+    main()
+
